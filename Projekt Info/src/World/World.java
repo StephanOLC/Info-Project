86a8bf2454cfdd8ca97dec26ae41bad8f39523c2
VitzthumDaniel <VitzthumDaniel@R227_P700I3_10.paed.local>
2017-05-12 09:12:15 +0200

    HitboxRegister implemented as part of World

---
+++ Projekt Info/src/World/World.java
@@ -0,0 +1,15 @@
+package World;
+
+import java.util.ArrayList;
+
+public class World {
+	
+	ArrayList<Hitbox> hitboxRegister;
+	
+	public World(){
+		hitboxRegister = new ArrayList<Hitbox>();
+	}
+	
+	
+	
+}
